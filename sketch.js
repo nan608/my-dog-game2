47ea299bf7558ab989fd2f09b3125c8a82e4d9d3
nan608 <83349564+nan608@users.noreply.github.com>
2021-09-24 20:48:11 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -20,14 +20,14 @@ function setup() {
  dog2.velocityX = 1;
  foodgroup = new Group()
 
- for(var i=0; i<45; i++){
-   if(i%3==0){
+ for(var i=0; i<1000; i++){
+   if(i%30==0){
   bone1=new Bone (newX,random([250,300,350,400,450,500,550]))
-  foodgroup.add(bone1.body)}
+  foodgroup.add(bone1.spt)}
 
-  if(i%7===0){
+  if(i%70===0){
   milk1=new Milk (newX,random([250,300,400,450,500,550]))
-  foodgroup.add(milk1.body)}
+  foodgroup.add(milk1.spt)}
 
   
  
@@ -44,7 +44,7 @@ function draw() {
  camera .position.x = dog2.x+200
   if(keyDown("right")){
    
-    dog2.x+=80;
+    dog2.x+=30;
   }
   
     if(frameCount%30===0){
